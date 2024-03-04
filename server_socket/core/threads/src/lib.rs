9447d03978c7e0666c5ac3ca184e00517455a1d3
Patryk Zajdel <57092803+xMOROx@users.noreply.github.com>
2024-03-04 19:18:23 +0100

    Create threadpool

--- server_socket/core/threads/src/lib.rs
+++ server_socket/core/threads/src/lib.rs
@@ -1,13 +1,80 @@
-pub struct ThreadPool {
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 struct Worker {
-
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
+impl ThreadPool {
+    pub fn build(number_of_threads:usize) -> Self {
+       Self::check_non_zero_value(number_of_threads);
 
+       let (sender, receiver) = mpsc::channel();
 
+       let receiver = Arc::new(Mutex::new(receiver));
+
+       let mut workers = Vec::with_capacity(number_of_threads);
+
+       for id in 0..number_of_threads {
+           workers.push(Worker::new(id, Arc::clone(&receiver)));
+       }
+
+       ThreadPool {
+           workers,
+           sender:Some(sender),
+       }
+    }
+
+    fn check_non_zero_value(arg:usize) {
+        if arg == 0 {
+            panic!("Argument cannot be zero");
+        }
+    }
+    
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv();
+
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
--- server_socket/server/src/main.rs
+++ server_socket/server/src/main.rs
@@ -1,7 +1,26 @@
+use std::net::{TcpListener, TcpStream};
 use cl_parser::Config;
+use threads::ThreadPool;
 use std::env;
-fn main() {
-    
-    let config = Config::build(env::args());
 
+fn main() {
+    let config = Config::build(env::args());
+    let server_listener = TcpListener::bind(build_socket(&config))
+        .expect("Unable to listen on given port");
+
+    println!("Launching server on address {} and port {}", config.ip_address, config.port);
+    
+    let pool = ThreadPool::build(config.number_of_threads);
+
+    for stream in server_listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+        });
+    }
+}
+
+
+fn build_socket(config:&Config) -> String {
+    format!("{}:{}", config.ip_address, config.port)
 }
