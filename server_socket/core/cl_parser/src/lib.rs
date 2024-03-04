cfdb0a67fadd59ede79ac1a3b5c488d6b6547ce4
Patryk Zajdel <57092803+xMOROx@users.noreply.github.com>
2024-03-04 18:45:01 +0100

    Refactore commandline parser

--- server_socket/core/cl_parser/src/lib.rs
+++ server_socket/core/cl_parser/src/lib.rs
@@ -1,33 +1,104 @@
-const DEFAULT_NUM_OF_THREADS:u32 = 5;
+use std::process;
 
+const DEFAULT_NUM_OF_THREADS:usize = 5;
+
+#[derive(Debug)]
 pub struct Config {
-    number_of_threads:u32,
+   pub number_of_threads:usize,
+   pub ip_address:String,
+   pub port:String,
 }
+
+struct ArgumentParser;
+struct ArgumentHelper;
 
 enum ParsingError {
     NegativeNumberOfThreads,
     NonNumberPassed,
+    NumberNotInRange,
 }
 
 impl Config {
-    pub fn build<T>(mut args:T) -> Self
+    pub fn build<T>(args:T) -> Self
     where 
         T: Iterator<Item = String>
     {
-        args.next();
+        let config = ArgumentParser::parse(args);
+
+
+        config
+    }
+}
+
+impl ArgumentHelper {
+    pub fn display_help() {
+        println!("Arguments format:");
+        println!(r#"server address ip server port [number_of_threads]"#);
+    }
+}
+
+impl ArgumentParser {
+
+    pub fn parse<T>(mut args:T) -> Config
+    where  
+        T:Iterator<Item = String> 
+    {
+        //skip because first argument is a filepath
+        args.next(); 
+        
+        let mut ip_address:String = "".to_string();
+
+        if let Some(arg) = args.next() {
+            let trimmed = arg.trim();
+            if trimmed == "--help" || trimmed == "-h" {
+                ArgumentHelper::display_help();
+                process::exit(0);
+            }
+            ip_address = trimmed.to_string();
+        }
+
+        let port = if let Some(arg) = args.next() {
+            Self::handle_port_argument(arg)
+        } else {
+            panic!("Not enough number of arguments");
+        };
+
         let number_of_threads = if let Some(arg) = args.next() {
-            Self::execute(arg)
+            Self::handle_number_of_threads_argument(arg)
         } else {
             DEFAULT_NUM_OF_THREADS
         };
 
         Config {
-            number_of_threads
+            ip_address,
+            port,
+            number_of_threads,
         }
     }
 
-    pub fn get_number_of_threads(&self) -> u32 {
-        self.number_of_threads
+    fn handle_port_argument(arg:String) -> String {
+        let value = if let Ok(value) = Self::parse_number_argument(arg) {
+            value
+        } else {
+            panic!("Argument have to be a number");
+        };
+
+        if let Err(ParsingError::NegativeNumberOfThreads) = Self::check_positiveness_of_argument(value) {
+           panic!("Argument cannot be negative"); 
+        }
+
+        if let Err(ParsingError::NumberNotInRange) = Self::check_number_in_range(value) {
+            panic!("Port number has to be in range 1024 to 65535");
+        }
+        value.to_string()
+    }
+
+    fn check_number_in_range(arg:i32) -> Result<(), ParsingError> {
+        if arg < 1024 || arg > 65535 {
+            Err(ParsingError::NumberNotInRange)
+        } else {
+            Ok(())
+        }
     }
 
     fn check_positiveness_of_argument(arg:i32) -> Result<(), ParsingError> { 
@@ -38,15 +109,8 @@ impl Config {
         }
     }
 
-    fn parse_argument(arg:String) -> Result<i32, ParsingError> {
-        match arg.parse::<i32>() {
-            Ok(value) => Ok(value),
-            Err(_) => Err(ParsingError::NonNumberPassed),
-        }
-    }
-
-    fn execute(arg:String) -> u32 {
-        let value = if let Ok(value) = Self::parse_argument(arg) {
+    fn handle_number_of_threads_argument(arg:String) -> usize {
+        let value = if let Ok(value) = Self::parse_number_argument(arg) {
             value
         } else {
             panic!("Argument have to be a number");
@@ -56,12 +120,16 @@ impl Config {
            panic!("Argument cannot be negative"); 
         }
 
-        value as u32
+        value as usize
     }
-} 
-
-
-
+    
+    fn parse_number_argument(arg:String) -> Result<i32, ParsingError> {
+        match arg.parse::<i32>() {
+            Ok(value) => Ok(value),
+            Err(_) => Err(ParsingError::NonNumberPassed),
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -70,28 +138,42 @@ mod tests {
     #[test]
     #[should_panic(expected = "Argument cannot be negative")]
     fn should_panic_when_argument_is_negative() {
-        let args = vec!["file".to_string(), "-1".to_string()].into_iter();
+        let args = vec!["file".to_string(), "address".to_string(), "8080".to_string(),"-1".to_string()].into_iter();
         Config::build(args);
     }
     
     #[test]
     #[should_panic(expected = "Argument have to be a number")]
-    fn should_panic_when_argument_is_not_a_number() {
-        let args = vec!["file".to_string(), "test".to_string()].into_iter();
+    fn should_panic_when_threads_number_argument_is_not_a_number() {
+        let args = vec!["file".to_string(), "address".to_string(), "8080".to_string(),"test".to_string()].into_iter();
+        Config::build(args);
+    }
+    
+    #[test]
+    #[should_panic(expected = "Argument have to be a number")]
+    fn should_panic_when_port_number_argument_is_not_a_number() {
+        let args = vec!["file".to_string(), "address".to_string(), "port".to_string(),"test".to_string()].into_iter();
+        Config::build(args);
+    }
+    
+    #[test]
+    #[should_panic(expected = "Port number has to be in range 1024 to 65535")]
+    fn should_panic_when_port_number_is_not_in_range() {
+        let args = vec!["file".to_string(), "address".to_string(), "1023".to_string(),"test".to_string()].into_iter();
         Config::build(args);
     }
 
     #[test]
     fn is_config_with_default_number_of_threads() {
-        let args = vec!["file".to_string()].into_iter();
+        let args = vec!["file".to_string(), "address".to_string(), "8080".to_string()].into_iter();
         let config = Config::build(args);
         assert_eq!(config.get_number_of_threads(), DEFAULT_NUM_OF_THREADS);
     }
     
     #[test]
     fn is_config_with_custom_number_of_threads() {
-        let args = vec!["file".to_string(), "10".to_string()].into_iter();
+        let args = vec!["file".to_string(), "address".to_string(), "8080".to_string(), "10".to_string()].into_iter();
         let config = Config::build(args);
-        assert_eq!(config.get_number_of_threads(), 10_u32);
+        assert_eq!(config.get_number_of_threads(), 10_usize);
     }
 }
--- server_socket/core/threads/src/lib.rs
+++ server_socket/core/threads/src/lib.rs
@@ -1,14 +1,15 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct ThreadPool {
+
 }
+
+struct Worker {
+
+}
+
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
