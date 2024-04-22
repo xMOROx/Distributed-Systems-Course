372327c39af848f0a582396e2ef5e6a3d74c4ba0
Patryk Zajdel <57092803+xMOROx@users.noreply.github.com>
2024-04-22 15:20:13 +0200

    Class implemention from protoc generated abstract classes

---
+++ Grpc/Server/src/main/java/pl/zajdel/patryk/Devices/CO2LevelSensorImpl.java
@@ -0,0 +1,44 @@
+package pl.zajdel.patryk.Devices;
+
+import com.google.protobuf.Empty;
+import io.grpc.stub.StreamObserver;
+import pl.zajdel.patryk.gen.SmartHome.C02LevelSafe;
+import pl.zajdel.patryk.gen.SmartHome.CO2Level;
+import pl.zajdel.patryk.gen.SmartHome.CO2LevelSensorGrpc;
+import pl.zajdel.patryk.utils.OperationHelper;
+
+public class CO2LevelSensorImpl extends CO2LevelSensorGrpc.CO2LevelSensorImplBase {
+    private static final int SAFE_LEVEL_THRESHOLD = 5000;
+    private static final int CHANGE_LEVEL_MIN_VALUE = -5;
+    private static final int CHANGE_LEVEL_MAX_VALUE = 5;
+    private static final double CHANGE_LEVEL_PROBABILITY = 0.8;
+    private int co2Level;
+
+    public CO2LevelSensorImpl(int co2Level) {
+        this.co2Level = co2Level;
+    }
+
+
+    @Override
+    public void getCO2LevelInPPM(Empty request, StreamObserver<CO2Level> responseObserver) {
+        int co2LevelDelta = Math.random() <= CHANGE_LEVEL_PROBABILITY ?
+                OperationHelper.getRandomIntFromRange(CHANGE_LEVEL_MIN_VALUE, CHANGE_LEVEL_MAX_VALUE) : 0;
+        int newCO2Level = co2Level + co2LevelDelta;
+
+        if (newCO2Level > 0) {
+            co2Level = newCO2Level;
+        }
+
+        CO2Level result = CO2Level.newBuilder().setPpm(co2Level).build();
+        responseObserver.onNext(result);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void isCO2LevelSafe(Empty request, StreamObserver<C02LevelSafe> responseObserver) {
+        boolean isSafe = co2Level <= SAFE_LEVEL_THRESHOLD;
+        C02LevelSafe result = C02LevelSafe.newBuilder().setSafe(isSafe).build();
+        responseObserver.onNext(result);
+        responseObserver.onCompleted();
+    }
+}
---
+++ Grpc/Server/src/main/java/pl/zajdel/patryk/Devices/FridgeImpl.java
@@ -0,0 +1,62 @@
+package pl.zajdel.patryk.Devices;
+
+import com.google.protobuf.Empty;
+import io.grpc.stub.StreamObserver;
+import pl.zajdel.patryk.gen.SmartHome.FridgeGrpc;
+import pl.zajdel.patryk.gen.SmartHome.Temperature;
+import pl.zajdel.patryk.utils.OperationHelper;
+
+public class FridgeImpl extends FridgeGrpc.FridgeImplBase {
+
+    private static double MIN_SUPPORTED_TEMPERATURE = -20;
+    private static double MAX_SUPPORTED_TEMPERATURE = 20;
+    private static final double CHANGE_TEMPERATURE_MIN_VALUE = 0.1;
+    private static final double CHANGE_TEMPERATURE_MAX_VALUE = 1.0;
+    private static final double CHANGE_TEMPERATURE_PROBABILITY = 0.3;
+    private float targetTemperature;
+    private float currentTemperature;
+
+    public FridgeImpl(float targetTemperature, float currentTemperature) {
+        this.targetTemperature = targetTemperature;
+        this.currentTemperature = currentTemperature;
+    }
+
+    @Override
+    public void setTargetTemperature(Temperature request, StreamObserver<Temperature> responseObserver) {
+        if (!checkIfTemperatureInSupportedRange(request.getTemperature())) {
+            throw new TemperatureOutOfSupportedRangeError();
+        }
+        targetTemperature = temperature;
+        responseObserver.onNext(Temperature.newBuilder().setTemperature(targetTemperature).build());
+    }
+
+    @Override
+    public void getTargetTemperature(Empty request, StreamObserver<Temperature> responseObserver) {
+        responseObserver.onNext(Temperature.newBuilder().setTemperature(targetTemperature).build());
+    }
+
+    @Override
+    public void getCurrentTemperature(Empty request, StreamObserver<Temperature> responseObserver) {
+        double temperatureDelta = Math.random() <= CHANGE_TEMPERATURE_PROBABILITY ?
+                OperationHelper.getRandomDoubleFromRange(CHANGE_TEMPERATURE_MIN_VALUE, CHANGE_TEMPERATURE_MAX_VALUE) : 0;
+
+        double missingTemperature = Math.abs(currentTemperature - targetTemperature);
+
+        if (missingTemperature < temperatureDelta) {
+            temperatureDelta = missingTemperature;
+        }
+        if (currentTemperature > targetTemperature) {
+            temperatureDelta *= -1;
+        }
+        double newTemperature = currentTemperature + temperatureDelta;
+        if (checkIfTemperatureInSupportedRange(newTemperature)) {
+            currentTemperature = (float) newTemperature;
+        }
+
+        responseObserver.onNext(Temperature.newBuilder().setTemperature(currentTemperature).build());
+    }
+
+    private boolean checkIfTemperatureInSupportedRange(double temperature) {
+        return OperationHelper.checkIfValueInRange(temperature, MIN_SUPPORTED_TEMPERATURE, MAX_SUPPORTED_TEMPERATURE);
+    }
+}
