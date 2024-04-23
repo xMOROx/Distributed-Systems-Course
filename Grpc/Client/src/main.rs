43eb24cb2378640654a7219d2426eb6238f74b76
Patryk Zajdel <57092803+xMOROx@users.noreply.github.com>
2024-04-23 20:24:56 +0200

    Init Rust client side

--- Grpc/Client/src/main.rs
+++ Grpc/Client/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+
+mod proto {
+    tonic::include_proto!("SmartHome");
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "http://[::1]:50051";
+
+    Ok(())
 }
