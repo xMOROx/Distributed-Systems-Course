69352369511d12c22bbe3fd976a99c9af984db07
Patryk Zajdel <57092803+xMOROx@users.noreply.github.com>
2024-04-24 12:45:15 +0200

    Implement main loop

--- Grpc/Client/src/main.rs
+++ Grpc/Client/src/main.rs
@@ -1,12 +1,68 @@
+use std::collections::HashMap;
+use proto::fridge_with_ice_cube_maker_client::FridgeWithIceCubeMakerClient;
+use crate::command_handler::{CommandsHandler, local_commands::LocalCommandsHandler, remote_commands::*};
 
 mod proto {
     tonic::include_proto!("smart_home");
 }
 
+mod command_parser;
+mod command_handler;
+
+mod settings {
+    pub const IP_ADDRESS: &str = "http://127.0.0.!!";
+    pub const PORT: &str = "50051";
+}
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "http://[::1]:50051";
+    let mut handlers: HashMap<String, CommandsHandler> = HashMap::new();
 
+    handlers.insert("!".to_string(), CommandsHandler::LocalCommandsHandler(LocalCommandsHandler));
+    handlers.insert("CO2LevelSensor".to_string(), CommandsHandler::CO2LevelSensorCommandsHandler(CO2LevelSensorCommandsHandler));
+    handlers.insert("FridgeWithIceCubeMaker".to_string(), CommandsHandler::FridgeWithIceCubeMakerCommandsHandler(FridgeWithIceCubeMakerCommandsHandler));
+    handlers.insert("SmartDevice".to_string(), CommandsHandler::SmartDeviceCommandsHandler(SmartDeviceCommandsHandler));
+    handlers.insert("FridgeWithShoppingList".to_string(), CommandsHandler::FridgeWithShoppingListCommandsHandler(FridgeWithShoppingListCommandsHandler));
+
+    loop {
+        println!("Enter command: ");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let mut handled = false;
+        let command = command_parser::CommandParser::parse(input);
+
+        println!("Command: {:?}", command);
+
+        let handler =  handlers.get(command.get_command());
+        if let Some(handler) = handler {
+            match handler {
+                CommandsHandler::LocalCommandsHandler(handler) => {
+                    handler.handle_command(&command).await.expect("TODO: panic message");
+                    handled = true;
+                }
+                CommandsHandler::CO2LevelSensorCommandsHandler(handler) => {
+                    handler.handle_command(&command).await.expect("TODO: panic message");
+                    handled = true;
+                }
+                CommandsHandler::FridgeWithIceCubeMakerCommandsHandler(handler) => {
+                    handler.handle_command(&command).await.expect("TODO: panic message");
+                    handled = true;
+                }
+                CommandsHandler::SmartDeviceCommandsHandler(handler) => {
+                    handler.handle_command(&command).await.expect("TODO: panic message");
+                    handled = true;
+                }
+                CommandsHandler::FridgeWithShoppingListCommandsHandler(handler) => {
+                    handler.handle_command(&command).await.expect("TODO: panic message");
+                    handled = true;
+                }
+            }
+        }
+
+        if !handled {
+            println!("Couldn't find a handler to handle the command");
+        }
+    }
     Ok(())
 }
